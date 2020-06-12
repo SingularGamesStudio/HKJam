d60e21f329b9e1745670e9d99e2a28c32b62e3de
straple <54230867+Straple@users.noreply.github.com>
2020-06-12 14:48:22 +0300

    Delete Prefabs.meta

---
+++ Assets/Scripts/Emove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Move{
+    public class Emove{
+        public static Vector3 simulate_move3d(Vector3 p, ref Vector3 dp, Vector3 ddp, float dt){
+            return new Vector3(simulate_move(p.x, ref dp.x, ddp.x, dt),
+                               simulate_move(p.y, ref dp.y, ddp.y, dt),
+                               simulate_move(p.z, ref dp.z, ddp.z, dt));
+        }
+        public static Vector2 simulate_move2d(Vector2 p, ref Vector2 dp, Vector2 ddp, float dt){
+            return new Vector2(simulate_move(p.x, ref dp.x, ddp.x, dt),
+                                      simulate_move(p.y, ref dp.y, ddp.y, dt));
+        }
+        public static float simulate_move(float p, ref float dp, float ddp, float dt){
+            ddp -= dp * 15f;
+            p = p + dp * dt + ddp * dt * dt * .5f;
+            dp = dp + ddp * dt;
+            return p;
+        }
+    }
+}
---
+++ Assets/Scripts/Step.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Move;
+
+public class Step : MonoBehaviour
+{
+    Vector2 dp;
+    GameObject obj;
+    public static Step _step;
+    Vector2 position;
+    enum type
+    {
+        Up,
+        Down,
+        Forward,
+        Back,
+        Null,
+    }
+    type Vertical, Horizontal;
+
+    // const variables
+    const float cell_len = 3f;
+    const float speed = 700f;
+
+    void Awake()
+    {
+        _step = this;
+        Vertical = Horizontal = type.Null;
+    }
+
+    public void simple_forward()
+    {
+        Horizontal = type.Forward;
+        position.x = obj.transform.position.x + cell_len;
+    }
+    public void simple_up()
+    {
+        Vertical = type.Up;
+        position.y = obj.transform.position.y + cell_len;
+    }
+    public void simple_down()
+    {
+        Vertical = type.Down;
+        position.y = obj.transform.position.y - cell_len;
+    }
+    public void forward()
+    {
+        if (obj.transform.position.y > 0)
+        { // мы находимся наверху
+            simple_down();
+        }
+        simple_forward();
+    }
+    public void up()
+    {
+        simple_forward();
+        simple_up();
+    }
+
+    public void new_game_ojbect(GameObject Game_object)
+    {
+        obj = Game_object;
+    }
+
+    float update_position(ref float position, float p, ref float dp, ref type Move)
+    {
+        if (Move != type.Null)
+        {
+            {
+                float ddp = 0f;
+                ddp = (Move == type.Up || Move == type.Forward ? speed : -speed);
+                p = Emove.simulate_move(p, ref dp, ddp, Time.deltaTime);
+            }
+            if (Move == type.Down || Move == type.Back)
+            {
+                if (p < position)
+                {
+                    p = position;
+                    Move = type.Null;
+                }
+            }
+            else if (Move == type.Up || Move == type.Forward)
+            {
+                if (p > position)
+                {
+                    p = position;
+                    Move = type.Null;
+                }
+            }
+        }
+        return p;
+    }
+
+    void Update()
+    {
+        obj.transform.position = new Vector2(
+            update_position(ref position.x, obj.transform.position.x, ref dp.x, ref Horizontal),
+            update_position(ref position.y, obj.transform.position.y, ref dp.y, ref Vertical));
+    }
+}
